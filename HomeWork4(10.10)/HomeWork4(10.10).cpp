5b2df0ea4118aed81a2e8cbdfbbd22b35ac6424d
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2023-10-08 12:56:15 +0200

    Add files via upload

---
+++ HomeWork4(10.10)/HomeWork4(10.10).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int Sum(int a)
+{
+    if (a > 0)
+    {
+        return(a % 10) + Sum(a / 10);
+    }   
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+    int sum = Sum(a);
+    cout << "Sum: " << sum-1;
+}
